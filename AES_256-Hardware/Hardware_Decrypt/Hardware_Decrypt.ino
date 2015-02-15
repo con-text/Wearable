59f4d1fcaf669efe1804021e33b14defd46226a3
Benji Barash <bbarash@gmail.com>
2015-02-15 13:03:53 +0000

    Small program to get status register val - doesn't work

--- AES_256-Hardware/Hardware_Decrypt/Hardware_Decrypt.ino
+++ AES_256-Hardware/Hardware_Decrypt/Hardware_Decrypt.ino
@@ -1,8 +1,9 @@
-#include <aes132.h>
-
 #include <Wire.h>
-#include <aes132_i2c.h>
+#include <aes132.h>
+//#include <aes132_i2c.h>
 #include <aes132_commands.h>
+
+byte val = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,8 +13,20 @@ void setup() {
 
   Serial.println("Starting I2C");
   Wire.begin();
-  i2c_enable(); 
-  calculateTemperature();
+  
+  uint8_t sla = 0x50;  
+  uint8_t statr[2] = {0xFF, 0xF0};
+  
+  Wire.beginTransmission(sla);
+  Wire.write(statr, (uint8_t)2);
+  int ret = Wire.endTransmission();
+  Serial.println(ret);
+
+  int available_bytes = Wire.requestFrom((int)sla, 1);
+  Serial.println(available_bytes);
+
+  //i2c_enable(); 
+  //getInfo();
 }
 
 void loop() {
@@ -26,6 +39,20 @@ byte calculateTemperature()
   uint16_t response[1] = {0};
   
   uint8_t ret_code = aes132m_temp_sense(response);
+  
+  for (int i = 0; i < sizeof(response); i++) {
+    Serial.print(response[i], HEX);
+  }
+  Serial.println();
+
+  return ret_code;  
+}
+
+byte getInfo()
+{
+  uint8_t response[1] = {0};
+  
+  uint8_t ret_code = aes132m_info(6, response);
   
   for (int i = 0; i < sizeof(response); i++) {
     Serial.print(response[i], HEX);
