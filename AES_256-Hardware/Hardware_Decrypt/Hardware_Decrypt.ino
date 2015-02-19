2cbc3daf78c1b59947efaf1c9c36bf5ae4b30323
Denis Ogun <do1303@my.bristol.ac.uk>
2015-02-19 13:24:08 +0000

    Made changes to the sketch

--- AES_256-Hardware/Hardware_Decrypt/Hardware_Decrypt.ino
+++ AES_256-Hardware/Hardware_Decrypt/Hardware_Decrypt.ino
@@ -23,12 +23,12 @@ void setup() {
   }
   
   uint8_t dataToDecrypt[] = {
-    0xD5, 0x02, 0x16, 0x39, 0x56, 0x0E, 0x28, 0x29,
+    0xD5, 0x02, 0x16, 0x39, 0x56, 0x0E, 0x28, 0x29, 
     0xF2, 0x97, 0x0F, 0x06, 0x18, 0x78, 0x52, 0xDF };
     
   uint8_t inMac[] = {
-    0xFE, 0xC1, 0x7B, 0x9E, 0x6C, 0x91, 0xB9, 0xFC, 
-    0x4A, 0x4F, 0x7D, 0x3A, 0x03, 0x90, 0x93, 0x4C };
+    0x27, 0x39, 0x6A, 0xBC, 0xC2, 0xFD, 0x88, 0x2C, 
+    0x15, 0xC7, 0xE5, 0x0D, 0x0B, 0x62, 0x6E, 0x24 };
     
   uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN+16] = {0};
   decrypt(dataToDecrypt, inMac, rxBuffer);
@@ -43,9 +43,7 @@ void setup() {
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
     0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
     
-    
-    
-  writeKey(keyOne, 0);*/
+  writeKey(keyOne, 0);
  // uint16_t address = 0xF200;
  // uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN + 16] = {0};
   //aes132m_block_read(address, 16, rxBuffer);
@@ -61,13 +59,18 @@ void setup() {
   
   /* Uncomment for encryption
   
-  uint8_t rxBuffer2[AES132_RESPONSE_SIZE_MIN + 32] = {0};
-  encrypt(rxBuffer2);
-  
-  delay(500);
+  uint8_t rxBuffer2[AES132_RESPONSE_SIZE_MIN] = {0};
+  nonce(rxBuffer2);
   
   for (int i = 0; i < sizeof(rxBuffer2); i++) {
     Serial.println(rxBuffer2[i], HEX);
+  }
+  
+  uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN + 32] = {0};
+  encrypt(rxBuffer);
+    
+  for (int i = 0; i < sizeof(rxBuffer); i++) {
+    Serial.println(rxBuffer[i], HEX);
   } */
  
    //getInfo();
