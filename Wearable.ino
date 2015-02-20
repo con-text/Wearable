8b9c14a7697129d922564e0b7efd74a5103b997c
Denis Ogun <do1303@my.bristol.ac.uk>
2015-02-20 13:37:42 +0000

    Added AES hardware

--- Wearable.ino
+++ Wearable.ino
@@ -2,15 +2,24 @@
   Context Project BLE Login Device
 */
 
+
+// Defines whether to use the AES crypto chip
+#define AES_HARDWARE 1
+#define AES_LIBRARY_DEBUG 1
+#define interval 5000
+
 // Wire library
 #include <Wire.h>
 // RFDuino Library
 #include <RFduinoBLE.h>
-// AES Software
-#include <aes256.h>
-// AES Hardware
-#include <aes132.h>
-#include <aes132_commands.h>
+
+// AES functions
+#if AES_HARDWARE
+  #include <aes132.h>
+  #include <aes132_commands.h>
+#else
+  #include <aes256.h>
+#endif
 // Sizes of types
 #include <limits.h>
 // FSM Library
@@ -20,7 +29,6 @@
 
 // Disconnect automatically after 5s
 elapsedMillis interruptTimer;
-#define interval 5000
 
 // Variables
 String inputString = "";
@@ -31,20 +39,22 @@ String serverCipher;
 bool cipherOK = false;
 
 // AES Encryption and Decryption
-aes256_context ctxt;
-uint8_t keyOne[] = {
+#if AES_HARDWARE == 0
+  aes256_context ctxt;
+  
+  uint8_t keyOne[] = {
   0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
   0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
   0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-  0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
-};
+  0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
 
-uint8_t keyTwo[] = {
+  uint8_t keyTwo[] = {
   0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
   0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
   0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-  0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
-};
+  0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
+
+#endif
 
 /* State machine */
 State Advertising = State(advertising, NULL, NULL);
@@ -94,8 +104,8 @@ void didConnect()
   Serial.println(F("---In connected state---"));
 
   String randomString = generate128BitRandom();
-  
-  if (randomString != "ERROR")
+
+  if (randomString == "ERROR")
   {
     Serial.println("Error generating random number, resetting");
     stateMachine.transitionTo(Advertising);
@@ -117,14 +127,35 @@ void waitingCipher()
 {
   if (serverCipher != "") {
     Serial.println(F("---Decoding received cipher--"));
-    uint8_t hexString[16];
+    
+    // Hardware has a MAC
+    #if AES_HARDWARE
+      uint8_t hexString[32];
+      uint8_t data[16];
+      uint8_t MAC[16];
+    #else
+      uint8_t hexString[16];
+    #endif
 
     // Convert the string to uint8
     uint8FromString(serverCipher, hexString);
-    decryptMessage(0, hexString);
+    
+    #if AES_HARDWARE
+      memcpy(&MAC, &hexString, 16);
+      memcpy(&data, &hexString[16], 16);  
+      String decryptedString = decryptMessage(0, data, MAC);
+    #else
+      decryptMessage(0, hexString);
+      String decryptedString = stringFromUInt8(hexString, 16);
+    #endif
+    
+    if (decryptedString == "ERROR")
+    {
+      Serial.println("Error generating cipher, resetting");
+      stateMachine.transitionTo(Advertising);
+    }
 
     // Convert the uint8 back to a string
-    String decryptedString = stringFromUInt8(hexString, 16);
     Serial.println(decryptedString);
 
     if (decryptedString == sentRandom) {
@@ -137,7 +168,7 @@ void waitingCipher()
       stateMachine.transitionTo(Advertising);
     }
   }
-  
+
   if (interruptTimer > interval) {
     Serial.println(F("Didn't receive ciphertext, disconnecting."));
     RFduinoBLE.end();
@@ -149,21 +180,31 @@ void waitingRandom()
 {
   if (serverRandom != "") {
     Serial.println(F("---Encrypting received random--"));
+    // Hardware has a MAC     
     uint8_t hexString[16];
 
     // Convert the string to uint8
     uint8FromString(serverRandom, hexString);
-    encryptMessage(0, hexString);
+    
+    #if AES_HARDWARE
+      String encryptedString = encryptMessageString(0, hexString);
+    #else
+      encryptMessage(0, hexString);
+      String encryptedString = stringFromUInt8(hexString, 16);
+    #endif
 
+    if (encryptedString == "ERROR")
+    {
+      Serial.println("Error generating cipher, resetting");
+      stateMachine.transitionTo(Advertising);
+    }
     // Convert the uint8 back to a string
-    String encryptedString = stringFromUInt8(hexString, 16);
-
     sendMessage(encryptedString);
     Serial.println("Sending encrypted string");
-        
+
     stateMachine.transitionTo(ResetVariables);
   }
-  
+
   if (interruptTimer > interval) {
     Serial.println(F("Didn't receive random challenge, disconnecting."));
     RFduinoBLE.end();
@@ -273,6 +314,128 @@ void RFduinoBLE_onReceive(char *data, int len)
 
 /* AES Encryption and Decryption */
 
+#if AES_HARDWARE
+
+/* AES Hardware functions */
+String generate128BitRandom()
+{
+  uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN + 16] = {0};
+  randomNumber(rxBuffer);
+
+  if (rxBuffer[1] != 0x00) {
+    return "ERROR";
+  }
+
+  // Remove the packet size and checksums
+  cleanupData(rxBuffer, sizeof(rxBuffer));
+
+  // Turn the data array into a string
+  String randomString = stringFromUInt8(rxBuffer, sizeof(rxBuffer));
+
+  return randomString;
+}
+
+String decryptMessage(int key, uint8_t dataToDecrypt[16], uint8_t inMac[16])
+{
+  // Send the nonce
+  uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN] = {0};
+  nonce(rxBuffer);
+  
+  if (rxBuffer[1] != 0x00) {
+    return "ERROR";
+  }
+  
+  // Send the decrypt
+  uint8_t rxBuffer2[AES132_RESPONSE_SIZE_MIN+16] = {0};
+  decrypt(inMac, dataToDecrypt, rxBuffer);
+  
+  if (rxBuffer[1] != 0x00) {
+    return "ERROR";
+  }
+  
+  // Remove the packet size and checksums
+  cleanupData(rxBuffer2, sizeof(rxBuffer2));
+    
+  // Turn the data array into a string
+  String decryptedMessage = stringFromUInt8(rxBuffer2, sizeof(rxBuffer2));
+
+  return decryptedMessage;
+}
+
+String encryptMessageString(int key, uint8_t data[16])
+{
+  // Send the nonce
+  uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN] = {0};
+  nonce(rxBuffer);
+  
+  if (rxBuffer[1] != 0x00) {
+    return "ERROR";
+  }
+  
+  uint8_t rxBuffer2[AES132_RESPONSE_SIZE_MIN + 32] = {0};
+  encrypt(data, rxBuffer2);
+    
+  if (rxBuffer2[1] != 0x00) {
+    return "ERROR";
+  }
+  
+  // Remove the packet size and checksums
+  cleanupData(rxBuffer2, sizeof(rxBuffer2));
+    
+  // Turn the data array into a string
+  String encryptedMessage = stringFromUInt8(rxBuffer2, sizeof(rxBuffer2));
+
+  return encryptedMessage;
+}
+
+#else
+
+/* AES Software functions */
+
+String generate128BitRandom()
+{
+  uint8_t data[16];
+
+  unsigned long randomValue = micros();
+  randomSeed(randomValue);
+
+  long r = random(LONG_MAX);
+  long r1 = random(LONG_MAX);
+  long r2 = random(LONG_MAX);
+  long r3 = random(LONG_MAX);
+
+  memcpy(data, &r, sizeof(long));
+  memcpy(data + 4, &r1, sizeof(long));
+  memcpy(data + 8, &r2, sizeof(long));
+  memcpy(data + 12, &r3, sizeof(long));
+
+  // Turn the data array into a string
+  String randomString = stringFromUInt8(data, sizeof(data));
+  
+  return randomString;
+}
+
+void decryptMessage(int key, uint8_t data[16])
+{
+  // Setup the keys
+  if (key == 0) {
+    aes256_init(&ctxt, keyOne);
+  } else if (key == 1) {
+    aes256_init(&ctxt, keyTwo);
+  } else {
+    Serial.println(F("Unknown key specified"));
+  }
+
+  // Do the actual decryption
+  PrintHex8(F("Encrypted data: "), data, sizeof(data) * 4);
+  Serial.println(F("---Decrypting---"));
+  aes256_decrypt_ecb(&ctxt, data);
+  PrintHex8(F("Unencrypted data: "), data, sizeof(data) * 4);
+
+  // Clean up
+  aes256_done(&ctxt);
+}
+
 void encryptMessage(int key, uint8_t data[16])
 {
   // Setup the keys
@@ -289,66 +452,27 @@ void encryptMessage(int key, uint8_t data[16])
   Serial.println(F("---Encrypting---"));
   aes256_encrypt_ecb(&ctxt, data);
   PrintHex8(F("Encrypted data: "), data, sizeof(data) * 4);
-  
-  // Clean up
-  aes256_done(&ctxt);
-}
-
-void decryptMessage(int key, uint8_t data[16])
-{
-  // Setup the keys
-  if (key == 0) {
-    aes256_init(&ctxt, keyOne);
-  } else if (key == 1) {
-    aes256_init(&ctxt, keyTwo);
-  } else {
-    Serial.println(F("Unknown key specified"));
-  }
-
-  // Do the actual encryption
-  PrintHex8(F("Encrypted data: "), data, sizeof(data) * 4);
-  Serial.println(F("---Decrypting---"));
-  aes256_decrypt_ecb(&ctxt, data);
-  PrintHex8(F("Unencrypted data: "), data, sizeof(data) * 4);
 
   // Clean up
   aes256_done(&ctxt);
 }
 
-/* AES Hardware functions */
-
-String generate128BitRandom()
-{
-  uint8_t rxBuffer[AES132_RESPONSE_SIZE_MIN+16] = {0};
-  randomNumber(rxBuffer);
-  
-  if (rxBuffer[1] != 0x00) {
-    return "ERROR";
-  } 
-  
-  // Remove the packet size and checksums
-  cleanupData(rxBuffer, sizeof(rxBuffer));
-  
-  // Turn the data array into a string
-  String randomString = stringFromUInt8(rxBuffer, sizeof(rxBuffer));
-  
-  return randomString;
-}
+#endif
 
 /* Utilities */
 
 void cleanupData(uint8_t *data, int dataLength)
 {
   data[0] = NULL;
-  data[dataLength-1] = NULL;
-  data[dataLength-2] = NULL; 
+  data[dataLength - 1] = NULL;
+  data[dataLength - 2] = NULL;
 }
 
 String stringFromUInt8(uint8_t* data, int dataLength)
 {
   String hexMessage = "";
   String newString;
-    
+
   for (int i = 0; i < dataLength; i++) {
     if (data[i] == NULL) {
       continue;
@@ -379,13 +503,13 @@ void uint8FromString(String message, uint8_t* data)
 void PrintHex8(const __FlashStringHelper* message, uint8_t *data, uint8_t length) // prints 8-bit data in hex with leading zeroes
 {
   Serial.println(message);
-  Serial.print("0x"); 
-  for (int i=0; i<length; i++) { 
-    if (data[i]<0x10) {
+  Serial.print("0x");
+  for (int i = 0; i < length; i++) {
+    if (data[i] < 0x10) {
       Serial.print("0");
-    } 
-    Serial.print(data[i],HEX); 
-    Serial.print(" "); 
+    }
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
   }
   Serial.println("");
 }
